c88ff483fe1f73563c8b8125350319ea2c259893
PlaNexq <48767800+PlaNexq@users.noreply.github.com>
2024-12-19 06:59:13 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace compGraphLab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBarX = new System.Windows.Forms.TrackBar();
+            this.trackBarY = new System.Windows.Forms.TrackBar();
+            this.trackBarZ = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarZ)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(338, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(450, 298);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // trackBarX
+            // 
+            this.trackBarX.Location = new System.Drawing.Point(34, 41);
+            this.trackBarX.Maximum = 180;
+            this.trackBarX.Minimum = -180;
+            this.trackBarX.Name = "trackBarX";
+            this.trackBarX.Size = new System.Drawing.Size(104, 45);
+            this.trackBarX.TabIndex = 1;
+            this.trackBarX.ValueChanged += new System.EventHandler(this.trackBarX_ValueChanged);
+            // 
+            // trackBarY
+            // 
+            this.trackBarY.Location = new System.Drawing.Point(34, 92);
+            this.trackBarY.Maximum = 180;
+            this.trackBarY.Minimum = -180;
+            this.trackBarY.Name = "trackBarY";
+            this.trackBarY.Size = new System.Drawing.Size(104, 45);
+            this.trackBarY.TabIndex = 2;
+            this.trackBarY.ValueChanged += new System.EventHandler(this.trackBarY_ValueChanged);
+            // 
+            // trackBarZ
+            // 
+            this.trackBarZ.Location = new System.Drawing.Point(34, 158);
+            this.trackBarZ.Maximum = 180;
+            this.trackBarZ.Minimum = -180;
+            this.trackBarZ.Name = "trackBarZ";
+            this.trackBarZ.Size = new System.Drawing.Size(104, 45);
+            this.trackBarZ.TabIndex = 3;
+            this.trackBarZ.ValueChanged += new System.EventHandler(this.trackBarZ_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBarZ);
+            this.Controls.Add(this.trackBarY);
+            this.Controls.Add(this.trackBarX);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarZ)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBarX;
+        private System.Windows.Forms.TrackBar trackBarY;
+        private System.Windows.Forms.TrackBar trackBarZ;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Numerics;
+using System.Windows.Forms;
+
+namespace compGraphLab1
+{
+    public partial class Form1 : Form
+    {
+        Pen meshPen;
+        Surface surface;
+        float RotationX = 0f;
+        float RotationY = 0f;
+        float RotationZ = 0f;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            meshPen = new Pen(Color.Black, 1);
+            GraphicsEngine.RecalcGeneral(RotationX, RotationY, RotationZ);
+
+            List<Vector3> points = new List<Vector3> { { new Vector3(50, 50, 2) }, { new Vector3(25, 25, 2) }, { new Vector3(50, 25, 2) } };
+            List<int> polys = new List<int> { 0, 1, 2 };
+
+
+            surface = new Surface(points, polys);
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            surface.DrawMesh(e.Graphics, meshPen);
+        }
+
+        private void trackBarX_ValueChanged(object sender, EventArgs e)
+        {
+            RotationX = (float)(Math.PI/180) * trackBarX.Value;
+            GraphicsEngine.RecalcGeneral(RotationX, RotationY, RotationZ);
+            pictureBox1.Refresh();
+
+        }
+
+        private void trackBarY_ValueChanged(object sender, EventArgs e)
+        {
+            RotationY = (float)(Math.PI / 180) * trackBarY.Value;
+            GraphicsEngine.RecalcGeneral(RotationX, RotationY, RotationZ);
+            pictureBox1.Refresh();
+
+        }
+
+        private void trackBarZ_ValueChanged(object sender, EventArgs e)
+        {
+            RotationZ = (float)(Math.PI / 180) * trackBarZ.Value;
+            GraphicsEngine.RecalcGeneral(RotationX, RotationY, RotationZ);
+            pictureBox1.Refresh();
+
+        }
+    }
+    static class Equations
+    {
+        public static List<List<Vector3>> GetTris(double R, double r, int u_count, int v_count, double limit_u, double limit_v)
+        {
+            var du = 360 / limit_u;
+            var dv = 360 / limit_v;
+            return new List<List<Vector3>> { };
+        }
+        public static List<List<Vector3>> Sphere()
+        {
+            return null;
+        }
+    }
+
+    class Surface
+    {
+        public List<Vector3> m_Points;
+        List<Vector3> m_TransformedPoints;
+        public List<PointF> m_ScreenPoints;
+        List<Polygon> m_Polygons;
+
+        Color InsideColour = Color.Red;
+        Color OutsideColour = Color.Blue;
+        public Surface(List<Vector3> points, List<int> polys)
+        {
+            m_Points = points;
+            m_Polygons = new List<Polygon>();
+            for (int i = 0; i < polys.Count / 3; i++)
+            {
+                var buf = new Polygon(polys.GetRange(i * 3, 3), this);
+                m_Polygons.Add(buf);
+            }
+            Calculate();
+        }
+        public void DrawMesh(Graphics g, Pen pen)
+        {
+            Calculate();
+
+            foreach (var tri in m_Polygons)
+            {
+                tri.DrawMesh(g, pen);
+            }
+        }
+        public void FillColour()
+        {
+
+        }
+
+        void Calculate()
+        {
+            m_TransformedPoints = new List<Vector3>();
+            for (int i = 0; i < m_Points.Count; i++)
+            {
+                var point = GraphicsEngine.General.Multiply(new Vector4(m_Points[i], 1));
+                m_TransformedPoints.Add(new Vector3(point.X, point.Y, point.Z));
+            }
+            m_ScreenPoints = new List<PointF>();
+
+            for (var i = 0; i < m_TransformedPoints.Count; i++)
+            {
+                m_ScreenPoints.Add(Project(m_TransformedPoints[i]));
+            }
+        }
+
+        PointF Project(Vector3 b)
+        {
+            var projMat = new Matrix4();
+
+
+            return new PointF(b.X, b.Y);
+        }
+    }
+
+    class Polygon
+    {
+        List<int> PointIndexes;
+        Surface parent;
+
+        public Polygon(List<int> pointIndexes, Surface parent)
+        {
+            PointIndexes = pointIndexes;
+            this.parent = parent;
+        }
+
+        public void DrawMesh(Graphics g, Pen pen)
+        {
+            var p = new PointF[3] { parent.m_ScreenPoints[PointIndexes[0]], parent.m_ScreenPoints[PointIndexes[1]], parent.m_ScreenPoints[PointIndexes[2]] };
+
+            g.DrawPolygon(pen, p);
+        }
+
+        void FillColour()
+        {
+
+        }
+    }
+
+    class Matrix4
+    {
+        public float[,] contents;
+        public Matrix4()
+        {
+            this.contents = new float[4, 4] { {0,0,0,0}, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+        }
+        public Matrix4(float[,] contents)
+        {
+            this.contents = contents;
+        }
+        public Vector4 Multiply(Vector4 b)
+        {
+            return new Vector4(
+                b.X * contents[0, 0] + b.Y * contents[0, 1] + b.Z * contents[0, 2] + b.W * contents[0, 3],
+                b.X * contents[1, 0] + b.Y * contents[1, 1] + b.Z * contents[1, 2] + b.W * contents[1, 3],
+                b.X * contents[2, 0] + b.Y * contents[2, 1] + b.Z * contents[2, 2] + b.W * contents[2, 3],
+                b.X * contents[3, 0] + b.Y * contents[3, 1] + b.Z * contents[3, 2] + b.W * contents[3, 3]);
+        }
+        public Matrix4 Multiply(Matrix4 m)
+        {
+            float[,] buf = contents;
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    buf[i, j] = calcCell(i, j, m.contents);
+            return new Matrix4(buf);
+        }
+        private float calcCell(int row, int column, in float[,] secondMat)
+        {
+            return contents[row, 0] * secondMat[0, column] + contents[row, 1] * secondMat[1, column] + contents[row, 2] * secondMat[2, column] + contents[row, 3] * secondMat[3, column];
+        }
+    }
+
+    static class GraphicsEngine
+    {
+        public static Matrix4 General = new Matrix4(new float[4, 4] { { 1, 0, 0, 0 },
+                                                                      { 0, 1, 0, 0 },
+                                                                      { 0, 0, 1, 0 },
+                                                                      { 0, 0, 0, 1 }}
+        );
+
+        public static void RecalcGeneral(float rotX, float rotY, float rotZ)
+        {
+            General = new Matrix4(new float[4, 4] { { 1, 0, 0, 0 },
+                                                    { 0, 1, 0, 0 },
+                                                    { 0, 0, 1, 0 },
+                                                    { 0, 0, 0, 1 }}
+            );
+            var Rx = new Matrix4();
+            Rx.contents[0, 0] = 1;
+            Rx.contents[1, 1] = (float)Math.Cos(rotX); Rx.contents[1, 2] = (float)Math.Sin(rotX);
+            Rx.contents[2, 1] = -(float)Math.Sin(rotX); Rx.contents[2, 2] = (float)Math.Cos(rotX);
+            Rx.contents[3, 3] = 1;
+
+            var Ry = new Matrix4();
+            Ry.contents[0, 0] = (float)Math.Cos(rotY); Ry.contents[0, 2] = -(float)Math.Sin(rotY);
+            Ry.contents[1, 1] = 1;
+            Ry.contents[2, 0] = (float)Math.Sin(rotY); Ry.contents[2, 2] = (float)Math.Cos(rotY);
+            Ry.contents[3, 3] = 1;
+
+            var Rz = new Matrix4();
+            Rz.contents[0, 0] = (float)Math.Cos(rotZ); Rz.contents[0, 1] = (float)Math.Sin(rotZ);
+            Rz.contents[1, 0] = -(float)Math.Sin(rotZ); Rz.contents[1, 1] = (float)Math.Cos(rotZ);
+            Rz.contents[2, 2] = 1;
+            Rz.contents[3, 3] = 1;
+
+            General = General.Multiply(Rx);
+            General = General.Multiply(Ry);
+            General = General.Multiply(Rz);
+
+        }
+
+        public static Vector3 Newell(Polygon p)
+        {
+            return Vector3.Zero;
+        }
+    }
+}
+
